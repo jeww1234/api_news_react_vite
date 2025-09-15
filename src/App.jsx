b94028f5b38e360858f92d119795c9f9e6abb978
unknown <jeww1234@gmail.com>
2025-09-15 19:57:36 +0900

    news

--- src/App.jsx
+++ src/App.jsx
@@ -7,9 +7,8 @@ import Header from "./components/Header"
 import Footer from "./components/Footer"
 
 function App() {
-  const API_KEY = "5ca19004f97f4f5f9d74d0fbb95b21dc";
   const [url, setUrl] = useState(
-    new URL(`https://newsapi.org/v2/top-headlines?country=us&apiKey=${API_KEY}`)
+    new URL("https://noona-times-be-5ca9402f90d9.herokuapp.com/top-headlines")
   );
   const [news, setNews] = useState([]);
   console.log("url", url);
