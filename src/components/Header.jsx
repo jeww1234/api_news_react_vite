07aef9750eca197fe0e55b5c04e84708e37efdc8
unknown <jeww1234@gmail.com>
2025-09-20 16:14:41 +0900

    14?

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,13 +3,36 @@ import Nav from "./Nav";
 import SearchNews from "./SearchNews";
 import SideNav from "./SideNav";
 
-const Header = ({ setUrl, setCategory, showSideNav, setShowSideNav, page, setPage, pageSize }) => {
-  const categoryList = ["Business", "Entertainment", "General", "Health", "Science", "Sports", "Technology"]
+const Header = ({
+  setUrl,
+  setCategory,
+  showSideNav,
+  setShowSideNav,
+  page,
+  setPage,
+  pageSize,
+}) => {
+  const categoryList = [
+    "Business",
+    "Entertainment",
+    "General",
+    "Health",
+    "Science",
+    "Sports",
+    "Technology",
+  ];
   return (
-    <div className="bg-[black]">
-      <div className="bg-[black] max-w-[1545px] w-[100%] h-[2.5vh] fixed flex items-center py-[1vh]">
-        <SideNav showSideNav={showSideNav} setShowSideNav={()=>setShowSideNav(prev=>!prev)} setCategory={setCategory} categoryList={categoryList}/>
-        <SearchNews setUrl={setUrl}/>
+    <div className="bg-[black] relative">
+      <div className="fixed">
+        <div className="bg-[black] min-w-[1545px] h-[2.5vh] py-[1vh] flex items-center">
+          <SideNav
+            showSideNav={showSideNav}
+            setShowSideNav={() => setShowSideNav((prev) => !prev)}
+            setCategory={setCategory}
+            categoryList={categoryList}
+          />
+          <SearchNews setUrl={setUrl} />
+        </div>
       </div>
       <div className="head-box flex flex-col items-center min-h-[450px]">
         <img
@@ -21,7 +44,14 @@ const Header = ({ setUrl, setCategory, showSideNav, setShowSideNav, page, setPag
         <p className="text-[white] italic font-[serif] text-[0.8em]">
           A Magazine of Politics and Culture
         </p>
-        <Nav setUrl={setUrl} setCategory={setCategory} categoryList={categoryList} page={page} setPage={setPage} pageSize={pageSize}/>
+        <Nav
+          setUrl={setUrl}
+          setCategory={setCategory}
+          categoryList={categoryList}
+          page={page}
+          setPage={setPage}
+          pageSize={pageSize}
+        />
       </div>
     </div>
   );
